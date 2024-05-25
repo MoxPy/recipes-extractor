910e411eb73c349888540dd0ffbbc2e5cb5c42bf
MoxPy <80635030+MoxPy@users.noreply.github.com>
2024-05-25 22:01:39 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,41 @@
+import requests
+from bs4 import BeautifulSoup
+
+# INSERT URLS HERE
+urls = ["https://cosylab.iiitd.edu.in/recipedb/search_recipeInfo/10222", "https://cosylab.iiitd.edu.in/recipedb/search_recipeInfo/10223"]
+
+for url in urls:
+    response = requests.get(url)
+    html_content = response.content
+
+    # INIT
+    soup = BeautifulSoup(html_content, "html.parser")
+
+    # RECIPE NAME
+    recipe_name = soup.find("h3").text.strip()
+    print("\nRecipe name:", recipe_name)
+
+    # NUTRIENTS
+    nutrient_table = soup.find("table", {"class": "highlight striped"})
+    if nutrient_table:
+        nutrient_rows = nutrient_table.find_all("tr")
+        for row in nutrient_rows[1:]:
+            nutrient_name = row.find_all("td")[0].text.strip()
+            nutrient_value = row.find_all("td")[1].text.strip()
+            print(f"{nutrient_name}: {nutrient_value}")
+
+    # INGREDIENTS
+    ingredient_table = soup.find("div", {"id": "ingredient_nutri"})
+    if ingredient_table:
+        ingredient_rows = ingredient_table.find("table").find_all("tr")
+        for row in ingredient_rows[1:]:
+            ingredient_name = row.find_all("td")[0].text.strip()
+            ingredient_quantity = row.find_all("td")[1].text.strip()
+            ingredient_unit = row.find_all("td")[2].text.strip()
+            ingredient_state = row.find_all("td")[3].text.strip()
+            ingredient_energy = row.find_all("td")[4].text.strip()
+            ingredient_carb = row.find_all("td")[5].text.strip()
+            ingredient_protein = row.find_all("td")[6].text.strip()
+            ingredient_lipids = row.find_all("td")[7].text.strip()
+
+            print(f"Ingredient: {ingredient_name}, Quantity: {ingredient_quantity}, Unit: {ingredient_unit}, State: {ingredient_state}, Energy: {ingredient_energy}, Carb: {ingredient_carb}, Protein: {ingredient_protein}, Lipids: {ingredient_lipids}")
